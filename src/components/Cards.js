a3421b03b3c513bf929d920055991792d4d59757
Weronika <WerRic96@protonmail.com>
2022-11-01 13:30:20 +0100

    Create array of colors

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import Card from "./Card";
 import "../styles/Cards.css";
 
 
 function Cards(props) {
-    const [cards, setCards] = useState(Array(10).fill(null));
+    const [cards, setCards] = useState(
+        [
+            'blue', 'red', 'green', 'cyan', 'yellow', 'magenta', 'black', 'gray',
+        ],
+    );
 
     return (
         <div className="cards-container">
-            
+            <Card></Card>
         </div>
     );
 }
