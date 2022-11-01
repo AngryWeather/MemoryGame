ba5c71eb502fcf35443ecb88876f0353cedf94ef
Weronika <WerRic96@protonmail.com>
2022-11-01 21:10:01 +0100

    Add more colors to the game

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./Card";
 import "../styles/Cards.css";
 
@@ -7,12 +7,24 @@ function Cards(props) {
     const [cards, setCards] = useState(
         [
             'blue', 'red', 'green', 'cyan', 'yellow', 'magenta', 'black', 'gray',
+            'aliceblue', 'antiquewhite', 'silver', 'white', 'maroon', 'purple',
+            'fuchsia', 'lime', 'olive', 'navy', 'teal', 'aqua', 'aquamarine',
+            'azure', 'beige', 'cadetblue', 'chocolate', 'coral', 'cornflowerblue',
+            'crimson', 'darkcyan', 'darkolivegreen', 'darkseagreen', 'dodgerblue',
+            'firebrick', 'gold', 'greenyellow', 'indigo', 'khaki', 'lavender',
+            'lightpink', 'lightsalmon', 'lightseagreen', 'orange', 'orangered',
+            'pink', 'royalblue', 'salmon', 'steelblue', 'tomato', 'turquoise',
         ],
     );
 
+    // useEffect(() => {
+    //     console.log(cards.length);
+    // })
+
     const shuffleCards = () => {
-        console.log('shuffle');
+        setCards(cards => [...cards], cards.sort(() => Math.random() - 0.5));
     }
+
 
     return (
         <div className="cards-container">
