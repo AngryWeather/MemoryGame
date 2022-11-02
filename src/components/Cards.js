45f9e9ab29370da3a9c0fbe619516a51e2eee42c
Weronika <WerRic96@protonmail.com>
2022-11-02 01:51:45 +0100

    Move code from cards to mainDiv

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,54 +4,18 @@ import "../styles/Cards.css";
 
 
 function Cards(props) {
-    const [cards, setCards] = useState(
-        [
-            'blue', 'red', 'green', 'cyan', 'yellow', 'magenta', 'black', 'gray',
-            'aliceblue', 'antiquewhite', 'silver', 'white', 'maroon', 'purple',
-            'fuchsia', 'lime', 'olive', 'navy', 'teal', 'aqua', 'aquamarine',
-            'azure', 'beige', 'cadetblue', 'chocolate', 'coral', 'cornflowerblue',
-            'crimson', 'darkcyan', 'darkolivegreen', 'darkseagreen', 'dodgerblue',
-            'firebrick', 'gold', 'greenyellow', 'indigo', 'khaki', 'lavender',
-            'lightpink', 'lightsalmon', 'lightseagreen', 'orange', 'orangered',
-            'pink', 'royalblue', 'salmon', 'steelblue', 'tomato', 'turquoise',
-        ],
-    );
 
-    const [usedCards, setUsedCards] = useState([]);
-    const [score, setScore] = useState(0);
-
-    const addCardToUsedCards = (text) => {
-        setUsedCards(usedCards => [...usedCards, text]);
-        setScore(usedCards.length);
-    }
-
-    const resetCards = () => {
-        setUsedCards([]);
-    }
-
-    const isUsed = text => {
-        return usedCards.includes(text);
-    }
-
- 
-    useEffect(() => {
-        shuffleCards();
-    }, []);
-
-    const shuffleCards = () => {
-        setCards(cards => [...cards], cards.sort(() => Math.random() - 0.5));
-    }
 
     return (
         <div className="cards-container">
-            {cards.map(card => {
+            {props.cards.map(card => {
 
                 return (
                     <Card
-                        resetCards={resetCards}
-                        isUsed={isUsed}
-                        addCardToUsedCards={addCardToUsedCards} 
-                        shuffleCards={shuffleCards} 
+                        resetCards={props.resetCards}
+                        isUsed={props.isUsed}
+                        addCardToUsedCards={props.addCardToUsedCards} 
+                        shuffleCards={props.shuffleCards} 
                         style={{backgroundColor: `${card}`}} 
                         text={card}>
                     </Card>
--- src/components/MainDiv.js
+++ src/components/MainDiv.js
@@ -1,11 +1,57 @@
 import Cards from "./Cards";
 import HighScores from "./HighScores";
 import "../styles/MainDiv.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function MainDiv(props) {
+    const [cards, setCards] = useState(
+        [
+            'blue', 'red', 'green', 'cyan', 'yellow', 'magenta', 'black', 'gray',
+            'aliceblue', 'antiquewhite', 'silver', 'white', 'maroon', 'purple',
+            'fuchsia', 'lime', 'olive', 'navy', 'teal', 'aqua', 'aquamarine',
+            'azure', 'beige', 'cadetblue', 'chocolate', 'coral', 'cornflowerblue',
+            'crimson', 'darkcyan', 'darkolivegreen', 'darkseagreen', 'dodgerblue',
+            'firebrick', 'gold', 'greenyellow', 'indigo', 'khaki', 'lavender',
+            'lightpink', 'lightsalmon', 'lightseagreen', 'orange', 'orangered',
+            'pink', 'royalblue', 'salmon', 'steelblue', 'tomato', 'turquoise',
+        ],
+    );
+
+    const [usedCards, setUsedCards] = useState([]);
+    const [score, setScore] = useState(0);
+
+    const addCardToUsedCards = (text) => {
+        setUsedCards(usedCards => [...usedCards, text]);
+        setScore(usedCards.length);
+    }
+
+    const resetCards = () => {
+        setUsedCards([]);
+    }
+
+    const isUsed = text => {
+        return usedCards.includes(text);
+    }
+
+ 
+    useEffect(() => {
+        shuffleCards();
+    }, []);
+
+    const shuffleCards = () => {
+        setCards(cards => [...cards], cards.sort(() => Math.random() - 0.5));
+    }
     return (
         <main>
-            <Cards></Cards>
+            <Cards
+                cards={cards}
+                resetCards={resetCards}
+                isUsed={isUsed}
+                addCardToUsedCards={addCardToUsedCards} 
+                shuffleCards={shuffleCards}
+            >
+            </Cards>
             <HighScores></HighScores>
         </main>
     );
